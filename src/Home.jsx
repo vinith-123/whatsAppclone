992cb0c8bc66c94a481ff2aed84ae71c1341f7df
vinith-123 <33572601+vinith-123@users.noreply.github.com>
2023-02-24 23:21:41 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import '../src/index.css';
 import {Link} from 'react-router-dom';
 import HelpCenter from './HelpCenter'
