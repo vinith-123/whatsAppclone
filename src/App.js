73694068f18a0ec61e3747009a202226f953a5b4
vinith-123 <33572601+vinith-123@users.noreply.github.com>
2023-02-24 23:20:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import Home from './Home'
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Download from './Download';
-// import { Link } from 'react-router-dom';
-import HelpCenter from './HelpCenter'
-import Privacy from './Privacy'
-import Download from './Download';
-import Blog from './Blog';
-
 
 function App() {
   return (
     <div className="App">
-   { <Home/> }
-    {/*<BrowserRouter>
-    <Routes>
-    <Route path='blog' element={<Blog/>} />
-    <Route path='helpcenter' element={<HelpCenter/>} />
-    <Route path='download' element={<Download/>} />
-    <Route path='privacy' element={<Privacy/>} />
-    </Routes></BrowserRouter>
-  <Privacy/>*/}
-
-  
+      { <Home/> }
     </div>
   );
 }
